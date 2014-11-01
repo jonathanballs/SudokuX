2ebb418ae27419e36e3c0987c76fbfddaebdcd22
Jonathan Balls <j.balls@btopenworld.com>
2014-11-01 16:08:40 +0000

    added coordinates struct for ncurses

--- sudokux.h
+++ sudokux.h
@@ -1,8 +1,14 @@
 //width of a sudoku puzzle
 #define SUDOKU_COLUMNS				9
 
+//number of columns of boxes (usually 3x3 areas)
+#define SUDOKU_BOX_COLUMNS			3
+
 //height of a sudoku puzzle
 #define SUDOKU_ROWS					9
+
+//number of rows of boxes
+#define SUDOKU_BOX_ROWS				3
 
 //total number of cells
 #define SUDOKU_CELLS				SUDOKU_ROWS * SUDOKU_COLUMNS
@@ -12,3 +18,9 @@
 
 //character used to signify empty cell
 #define EMPTY_CELL					'.'
+
+struct coordinates
+{
+	int x;
+	int y;
+};
